fd094d4a7a34188790dd274667eda514b3102452
arivusurya <arivusurya.3@gmail.com>
2023-02-24 12:07:16 +0530

    test case 1 is completed

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace TestMaximum
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+
+            testCases<int> test = new testCases<int>();
+            Console.WriteLine("Test Case(1) ");
+            Console.WriteLine(test.maxi(5, 4, 3));
+            Console.WriteLine("Test Case(2) ");
+            Console.WriteLine(test.maxi(55, 89, 73));
+            Console.WriteLine("Test Case(3) ");
+            Console.WriteLine(test.maxi(101, 447, 833));
+
+
+
+        }
+    }
+}
