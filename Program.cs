529e2ac2af45b9badedd4e0c7774753f6ac79d94
arivusurya <arivusurya.3@gmail.com>
2023-02-24 12:18:04 +0530

    test case for float is completed

--- Program.cs
+++ Program.cs
@@ -8,13 +8,13 @@ namespace TestMaximum
         public static void Main(String[] args)
         {
 
-            testCases<int> test = new testCases<int>();
+            TestCases<float> test = new TestCases<float>();
             Console.WriteLine("Test Case(1) ");
-            Console.WriteLine(test.maxi(5, 4, 3));
+            Console.WriteLine(test.maxf(5.5F, 4.4F, 3.73F));
             Console.WriteLine("Test Case(2) ");
-            Console.WriteLine(test.maxi(55, 89, 73));
+            Console.WriteLine(test.maxf(55.8F, 89.76F, 73.66F));
             Console.WriteLine("Test Case(3) ");
-            Console.WriteLine(test.maxi(101, 447, 833));
+            Console.WriteLine(test.maxf(101.56F, 447.01F, 833.09F));
 
 
 
